4181c0cea72c087000baeb6845b010e0016c7c34
andres <andres@afituni-n3tchte.af.uc.edu>
2017-10-01 21:40:07 -0400

    adding dropdown menus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './src/app/nav-bar/nav-bar.component';
+import { HomeContentComponent } from './src/app/home-content/home-content.component';
+
+import { DropdownModule } from "ng2-dropdown";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
